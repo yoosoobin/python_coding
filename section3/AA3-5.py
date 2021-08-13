6c415932ddb82ea03aad97bcb577f3025bca047d
unknown <yoosoobin1997@gmail.com>
2021-08-13 22:23:36 +0900

    commit

---
+++ section3/AA3-5.py
@@ -0,0 +1,28 @@
+import sys
+#sys.stdin = open('input.txt','rt')
+n,m = map(int,sys.stdin.readline().split())
+a = list(map(int,sys.stdin.readline().split()))
+lt = 0
+rt = 1
+tot = a[0]
+cnt = 0
+
+while True:
+    if tot<m:
+        if rt<n:
+            tot +=a[rt]
+            rt+=1
+        else:
+            break
+    elif tot ==m:
+        cnt+=1
+        tot -= a[lt]
+        lt+=1
+    else:
+        tot-=a[lt]
+        lt +=1
+print(cnt)
+
+
+
+
