e6617dc1d64ae4fa7d35cf86c37f4ee649379ef6
unknown <yoosoobin1997@gmail.com>
2021-08-13 21:12:15 +0900

    commit

--- section3/AA3-4.py
+++ section3/AA3-4.py
@@ -4,7 +4,22 @@ fn = int(input())
 f = list(map(int,input().split()))
 sn = int(input())
 s = list(map(int,input().split()))
+sum_num = []
+p1,p2 = 0,0
+while True:
+    if p1==fn:
+        for i in s[p2:]:
+            sum_num.append(i)
+        break
+    elif p2 == sn:
+        for i in f[p1:]:
+            sum_num.append(i)
+    elif f[p1] <= s[p2]:
+        sum_num.append(f[p1])
+        p1 +=1
+    else:
+        sum_num.append(s[p2])
+        p2 +=1
 
-sum_list = f+s
-for i in sorted(sum_list):
+for i in sum_num:
     print(i,end=' ')
