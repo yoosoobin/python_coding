d97370e672d3b42976bf7bdc724f5dc479a91fee
unknown <yoosoobin1997@gmail.com>
2021-08-24 16:43:03 +0900

    commit

---
+++ section5/AA5-1.py
@@ -0,0 +1,18 @@
+import sys
+#sys.stdin=open("input.txt", "r")
+num, m = map(int,input().split())
+num = list(map(int,str(num)))
+stack = []
+
+
+for i in num:
+    while stack and m>0 and stack[-1]<i:
+        stack.pop()
+        m-=1
+    stack.append(i)
+
+if m!=0:
+    stack = stack[:-m]
+
+res = ''.join(map(str,stack))
+print(res)
