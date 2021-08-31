78a34582d3f77408bb4981b0b40e10ba9fff30cd
unknown <yoosoobin1997@gmail.com>
2021-08-31 15:19:38 +0900

    commit

---
+++ section5/AA5-11_2.py
@@ -0,0 +1,27 @@
+import sys
+import heapq
+#sys.stdin=open("input.txt", "r")
+
+mx = 0
+heap =[]
+heapq.heapify(heap)
+
+while True:
+    a = int(input())
+    if a== -1:
+        break
+    elif a!=0:
+        heapq.heappush(heap,-a)
+    else:
+        if len(heap) == 0:
+            print(-1)
+        else:
+            print(-heapq.heappop(heap))
+
+
+
+
+
+        
+
+
