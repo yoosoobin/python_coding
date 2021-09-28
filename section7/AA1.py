31d326d395317705848d9d0730a91593e6eaf40d
unknown <yoosoobin1997@gmail.com>
2021-09-28 16:07:53 +0900

    commit

---
+++ section7/AA1.py
@@ -0,0 +1,50 @@
+## 최대점수 구하기(DFS)
+
+import sys
+#sys.stdin=open("input.txt", "r")
+def DFS(L,sum, time):
+    global res
+    if time>m:
+        return
+    if L == n:
+        if sum>res:
+            res=sum
+    else:
+        DFS(L+1,sum+pv[L],time+pt[L]) #문제를 풀 경우
+        DFS(L+1,sum,time)
+        
+if __name__ == '__main__':
+    n,m = map(int,input().split())
+    pv=list()
+    pt=list()
+    for i in range(n):
+        a,b = map(int,input().split())
+        pv.append(a)
+        pt.append(b)
+    res =-2147000000
+    DFS(0,0,0) #LEVEL,SUM,TIME
+    print(res)
+
+
+
+    
+
+
+    
+    
+    
+    
+
+   
+
+
+ 
+
+
+
+
+
+
+        
+
+
